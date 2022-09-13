59b4c59b8300347138073990e6956a36a595da4c
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-13 13:58:09 +0200

    Add project files.

---
+++ groupwork/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void ArrayEx()
+{
+    string[] names = new string[5];
+    string[] name = { "Natalia", "Sayeh", "Sita", "Sirisha" }
+    string[] n=n
+
+
+    //sort
+
+}
